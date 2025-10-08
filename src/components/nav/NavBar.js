67598e7c483ea7e2b138d9e074a8eb5d0f26fe8d
flueshot <flueshot9@gmail.com>
2025-10-08 09:32:49 +0200

    added new navbar component and a branch

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,21 @@
+﻿import React from "react";
+import {
+    NavBar,
+    Collapse,
+    Typography,
+    IconButton,
+} from "@material-tailwind/react";
+
+
+
+
+function NavBar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default NavBar;
